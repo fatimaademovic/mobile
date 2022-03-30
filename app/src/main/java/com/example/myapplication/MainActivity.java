f4d2eb5d734f7625a42654d4fead8b3d4736d5e9
fatimaademovic <fatima.ademovic@stu.ibu.edu.ba>
2022-03-30 14:24:46 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view){
+
+    }
+    public void sendMedssage(View view){
+
+        Intent namjera = new Intent( this, display.class);
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        String message = editText.getText().toString();
+        namjera.putExtra(EXTRA_MESSAGE, message);
+        startActivity(namjera);
+
+
+
+    }
+
+}
